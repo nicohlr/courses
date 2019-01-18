b911d8c68cb03255aef500c922909cfb29b58142
Nicolas <nicolas.houlier@gmail.com>
2019-01-18 23:03:29 +0100

    syntax

--- Udemy/Deep_Learning_A-Z/homeworks/ann_homework_script.py
+++ Udemy/Deep_Learning_A-Z/homeworks/ann_homework_script.py
@@ -5,20 +5,21 @@ import pandas as pd
 import seaborn as sns
 
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 from sklearn.metrics import confusion_matrix
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 import keras
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
+from keras.wrappers.scikit_learn import KerasClassifier
 
 # Ignore warnings
 import warnings
 warnings.filterwarnings('ignore')
 
-# Data preprocessing
-
+# Data pre-processing
+current_path = os.path.dirname(os.path.dirname(os.path.abspath('__file__')))
 path_train = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath('__file__'))), 'ressources/Artificial_Neural_Networks/Churn_Modelling.csv')
 dataset = pd.read_csv(path_train)
 print(dataset.head())
@@ -26,6 +27,15 @@ print(dataset.info())
 
 X = dataset.iloc[:, 3:13].values # we modify indexes according to what we saw with the info() method of the dataset
 y = dataset.iloc[:, 13].values # idem
+
+# Encoding categorical data
+labelencoder_X_geo = LabelEncoder()
+X[:, 1] = labelencoder_X_geo.fit_transform(X[:, 1])
+labelencoder_X_gender = LabelEncoder()
+X[:, 2] = labelencoder_X_gender.fit_transform(X[:, 2])
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
@@ -40,7 +50,7 @@ X_test = sc.transform(X_test)
 # Initializing our ANN
 classifier = Sequential()
 # Adding the input layer and the first hidden layer of our ANN
-classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu', input_shape = (11,)))
+classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu', input_shape=(11,)))
 # Adding the second hidden layer
 classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu'))
 # Adding the output layer
@@ -52,7 +62,7 @@ classifier.fit(X_train, y_train, batch_size=10, epochs=100)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
-y_pred = (y_pred > 0.5) # convert probabilities to binary output
+y_pred = (y_pred > 0.5)  # convert probabilities to binary output
 
 # Making the Confusion Matrix
 cm = confusion_matrix(y_test, y_pred)
@@ -61,10 +71,11 @@ sns.heatmap(cm, annot=True, fmt=".3f", linewidths=.5, square=True, cmap='Blues_r
 plt.ylabel('Actual exited label')
 plt.xlabel('Predicted exited label')
 fig.xaxis.set_ticklabels(['Stay', 'Leave'])
-fig.yaxis.set_ticklabels(['Stay', 'Leave']);
+fig.yaxis.set_ticklabels(['Stay', 'Leave'])
 plt.show()
 
 # K-fold cross validation
+
 
 def build_ann():
     classifier = Sequential()
@@ -73,6 +84,7 @@ def build_ann():
     classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
     classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
     return classifier
+
 
 classifier = KerasClassifier(build_fn=build_ann, batch_size=10, epochs=100, verbose=0)
 accuracies = cross_val_score(estimator=classifier, X=X_train, y=y_train, cv=10, n_jobs=1)
@@ -99,6 +111,7 @@ classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accur
 
 # Model tuning
 
+
 def build_ann(optimizer):
     classifier = Sequential()
     classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu', input_shape = (11,)))
@@ -109,11 +122,12 @@ def build_ann(optimizer):
     classifier.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
     return classifier
 
+
 classifier = KerasClassifier(build_fn=build_ann, verbose=1)
 parameters = {
-    'batch_size' : [25, 38],
-    'epochs' : [100, 510],
-    'optimizer' : ['adam', 'rmsprop']
+    'batch_size': [20, 30, 40],
+    'epochs': [500],
+    'optimizer': ['adam', 'rmsprop']
 }
 grid_search = GridSearchCV(estimator=classifier, param_grid=parameters, scoring='accuracy', cv=5)
 grid_search.fit(X_train, y_train)

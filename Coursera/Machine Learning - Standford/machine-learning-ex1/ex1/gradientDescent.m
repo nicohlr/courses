ff4bbd728fa02415a2c32e21d8c6a98ade46d476
Nicolas <nicolas.houlier@gmail.com>
2019-02-14 14:56:15 +0100

    add gradient descent function

--- Coursera/Machine Learning - Standford/machine-learning-ex1/ex1/gradientDescent.m
+++ Coursera/Machine Learning - Standford/machine-learning-ex1/ex1/gradientDescent.m
@@ -16,16 +16,22 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
-
+    
+    n = length(theta); % number of variables
+    temp = zeros(1, n);
+    h_x = theta'*X';
+    
+    for j = 1:n % iterates over variables
+        derivative = sum((h_x' - y).*X(:,j));
+        theta_j = theta.'(1, j) - (alpha/m)*derivative; % compute new theta using gradient descent
+        temp(1, j) = theta_j; % store new values of theta in temporary array
+    end
+    
+    theta = temp.'; % update all theta simultaneously
+    
     % ============================================================
 
-    % Save the cost J in every iteration    
+    % Save the cost J in every iteration   
     J_history(iter) = computeCost(X, y, theta);
 
 end
